f63f919b2ddc7073782977e9bcfe66105bc61612
cib2000 <952891485@qq.com>
2018-11-01 11:09:06 +0800

    normal method

---
+++ src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] ars){
+        人 人=new 人();
+        人.说话();
+    }
+}
---
+++ src/人.java
@@ -0,0 +1,5 @@
+public class 人 {
+    public void 说话() {
+    System.out.println("我是人.");
+    }
+}
